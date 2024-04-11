0601f770f130a935d0e0b8cf8b3ec7083ef9c5d4
steniogobira <steniogobira@gmail.com>
2024-04-11 18:52:26 -0300

    Implementação de gráficos. Ajustes no Sidebar

--- dash.py
+++ dash.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import folium
+import plotly.express as px
+import millify 
 
 #import csv
 
@@ -14,21 +15,153 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
-#Side bar
+#Side bar (Filters)
 
 with st.sidebar:
     st.image('logo.png')
 
-    st.header("Filtros", divider='blue', help='Escolha aqui as características da pessoa cliente')
+    st.header("Filtros", divider='blue', help='Por favor, selecione as características dos clientes cujos dados serão apresentados.')
 
+    #Age
     list_age = df['Age'].sort_values(ascending=True).unique()
-    select_box_age = st.selectbox('Idade',list_age)
-
+    list_age = list_age.tolist()
+    selecte_slide_age = st.slider('Idade', value=[list_age[0],list_age[-1]], help=f'Inclua idades entre {list_age[0]} e {list_age[-1]} anos')
+    selected_age_range = range(selecte_slide_age[0],selecte_slide_age[-1])
+    
+    #Gender
     list_gender = df['Gender'].sort_values(ascending=True).unique()
-    select_box_gender = st.selectbox('Gênero',list_gender)
+    selected_gender = st.multiselect('Gênero',list_gender, default=list_gender, help='Selecione ao menos um gênero da lista')
+
+    #Status
+    selected_status_customer = st.selectbox('Status do Cliente', ['Todos','Ativo','Inativo'], help='O campo status indica se o cliente está ativo ou inativo no banco')
+    if selected_status_customer == 'Todos':
+        selected_status_customer = [0,1]
+    elif selected_status_customer == 'Ativo':
+        selected_status_customer = [1]
+    else: selected_status_customer = [0]
+
+filters = (df['Age'].isin(selected_age_range)) & (df['Gender'].isin(selected_gender)) & (df['IsActiveMember'].isin(selected_status_customer))
+
+try:
+    #Layout
+    col = st.columns((0.50, 1, 0.7), gap='large')
+
+    #collumn 1
+
+    with col[0]:
+        
+        #Total Customers
+        total_customer = df[filters]['CustomerId'].count()
+        if total_customer != 0:
+            st.metric('Total de Clientes', millify.millify(total_customer))
+
+        #Saldo Médio dos Clientes
+        mean_balance = round(df[filters]['Balance'].mean(), 2)
+        if mean_balance > 0 or mean_balance <= 0:
+            st.metric('Média de Saldo em Conta',f'U$ {mean_balance}')
+        
+        #plot bar
+        df_country_count = df[filters].groupby(['Geography'])[['CustomerId']].count().reset_index()
+        df_country_count.rename({'CustomerId': 'Clientes',
+                                'Geography': 'Pais'
+                                }, inplace=True, axis=1)
+        if len(selected_gender) > 0:
+            bar = px.bar(df_country_count, title='Clientes por Paises', x='Pais', y='Clientes')
+
+        st.plotly_chart(bar,use_container_width=True)
+    
+        #Plot pie_age_card
+        total_hasCard = df[filters & (df['HasCrCard'] == 1)]['CustomerId'].count()
+        percen_hascard = round((total_hasCard/total_customer)*100)
+        percen_nohascard = round(100 - percen_hascard)
+
+        data = {
+        "Categoria": ["Sim", "Não"],
+        "Porcentagem": [percen_hascard, percen_nohascard]
+        }
+        data_sorted = data.copy()
+        data_sorted['Porcentagem'] = sorted(data_sorted['Porcentagem'], reverse=True)
+
+        pie_age_card = px.pie(data, title= "Clientes com Cartão de Crédito" ,values='Porcentagem', names='Categoria', hole=0.8,)
+        pie_age_card.update_traces(hoverinfo='label+percent', textinfo='percent', textfont_size=20,
+                            marker=dict(line=dict(color='#000000', width=2)), 
+                            textposition='inside')
+        st.plotly_chart(pie_age_card,use_container_width=True)
 
 
-#Plot
-df_country_count = df[(df['Age'] == select_box_age) & (df['Gender'] == select_box_gender) ].groupby(['Geography'])[['CustomerId']].count().reset_index()
-df_country_count.rename({'CustomerId': 'Total Customer'}, inplace=True, axis=1)
-st.bar_chart(df_country_count, x='Geography', y='Total Customer')
+
+
+    with col[1]:
+
+        
+        #plot bar
+
+        #plot line
+        df_age_product = df[filters].groupby('Age')[['NumOfProducts']].sum().reset_index()
+        df_age_product.rename({'Age': 'Idade',
+                                'NumOfProducts': 'Número de Produtos'
+                                }, inplace=True, axis=1)
+        
+
+        line = px.line(df_age_product,title='Idade vs Produtos Adquiridos', x='Idade', y='Número de Produtos')
+        st.plotly_chart(line,use_container_width=True)
+
+        #plot line 2
+        df_age_score = df[filters].groupby('Age')[['CreditScore']].sum().reset_index()
+        df_age_score.rename({'Age': 'Idade',
+                                'CreditScore': 'Score'
+                                }, inplace=True, axis=1)
+        
+        line = px.line(df_age_score,title='Idade vs Produtos Adquiridos', x='Idade', y='Score')
+        st.plotly_chart(line,use_container_width=True)
+
+    with col[2]:
+        
+        st.markdown('### Top 10 Melhores Scores')
+        df_score = df[filters][['Surname', 'CreditScore']].sort_values(by=['CreditScore','Surname'], ascending=False).head(10).reset_index(drop=True)
+        df_score.rename({'Surname': 'Nome',
+                        'CreditScore': 'Score'}, axis=1, inplace=True)
+        top_10 = [i for i in range(1,11)]
+        df_score.index = top_10
+        st.dataframe(
+            df_score,use_container_width=True, 
+            column_config={
+            "Score": st.column_config.ProgressColumn(
+                "Score",
+                format='%d',
+                min_value=0,
+                max_value=1000,
+            )
+        }
+        )
+
+        st.markdown('### Top 10 Piores Scores')
+        df_score = df[filters][['Surname', 'CreditScore']].sort_values(by=['CreditScore','Surname'], ascending=True).head(10).reset_index(drop=True)
+        df_score.rename({'Surname': 'Nome',
+                        'CreditScore': 'Score'}, axis=1, inplace=True)
+        top_10 = [i for i in range(1,11)]
+        df_score.index = top_10
+        st.dataframe(
+            df_score,use_container_width=True, 
+            column_config={
+            "Score": st.column_config.ProgressColumn(
+                "Score",
+                format='%d',
+                min_value=0,
+                max_value=1000,
+            )
+        }
+        )
+except:
+
+    st.title('Opa! Pareceu que há algo de errado 🚨')
+    if (df['Age'].isin(selected_age_range).any()) == False:
+       st.subheader(f"""
+                    Não existe dados para o range de idade. 
+                    Por favor, selecione um intervalo que inclua idades entre {list_age[0]} e {list_age[-1]} anos"""
+                    )
+    elif (len(selected_age_range) == 0):
+       st.subheader(" Selecie um range de idade válido")
+    elif len(selected_gender) == 0:
+       st.subheader("Selecie ao menos um Gênero")
+    
